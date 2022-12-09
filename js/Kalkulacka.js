fd1ae049810660d8020440335a6eb6246667a2de
JackJayJames <ciklops@seznam.cz>
2022-12-09 23:52:24 +0100

    třída Kalkulačka s konstruktorem a onclick

--- js/Kalkulacka.js
+++ js/Kalkulacka.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class Kalkulacka {
+    constructor(){
+        this._tlacitka = document.querySelectorAll(".tlacitka");
+        this.output = document.querySelector("#output");
+
+        this._obsluhaTlacitek();
+    }
+    _obsluhaTlacitek(){
+        for(let button of this._tlacitka){
+            button.onclick = () => {
+            };
+        }
+    }
+}
